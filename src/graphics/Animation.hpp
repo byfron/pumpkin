baf2f1470313f84aa1cf50be7b892c8b9582f0f1
Jose C. Rubio <jose.rubio@mindmaze.ch>
2017-01-30 20:16:52 +0100

    Fixes walk animation

--- src/graphics/Animation.hpp
+++ src/graphics/Animation.hpp
@@ -7,11 +7,11 @@
 namespace pumpkin {
 
 class AnimationFactory;
-	
+
 class Animation : public GraphicsObject {
 
-public:	
-	
+public:
+
 	// Load from data. TODO: refactor this to TextureAtlas?
 	struct AtlasFrame {
 		AtlasFrame(float x, float y, bool f) : atlas_offset_x(x),
@@ -20,9 +20,9 @@ public:
 		float atlas_offset_x;
 		float atlas_offset_y;
 		bool flipped;
-       
+
 	};
-	
+
 	Animation(std::string anim_cfg);
 
 	void updateAtlasOffset() {
@@ -35,22 +35,23 @@ public:
 	void init() {
 		GraphicsObject::init();
 	}
-	
+
 	bool isValid() {
 		return m_animation.count(m_active_anim) &&
 			m_animation[m_active_anim].size() > m_active_frame;
-	}       
+	}
 
-	void update(float dt) {	      	       
+	void update(float dt) {
 		if (isTimeToSwitchFrames(dt) && m_enabled) { frameForward(); }
 		GraphicsObject::update(dt);
 	}
-	
+
 	void addFrame(uint32_t anim_id, const AtlasFrame & frame) {
 			m_animation[anim_id].push_back(frame);
 	}
 
 	void switchToAnim(uint32_t id) {
+		m_enabled = true;
 		if (id != m_active_anim) {
 			m_active_anim = id;
 			m_active_frame = 0;
@@ -72,20 +73,24 @@ public:
 		if (m_active_frame >= m_animation[m_active_anim].size() - 1) {
 			m_active_frame = 0;
 		}
-		else m_active_frame++;		
-		updateAtlasOffset();				
+		else m_active_frame++;
+		updateAtlasOffset();
 	}
-	
+
+	void stop() {
+		m_enabled = false;
+	}
+
 protected:
 
 	float m_time_since_last_anim;
-	float m_frame_period;	
+	float m_frame_period;
 	int16_t m_active_anim;
 	int16_t m_active_frame;
 	bool m_enabled;
-	
+
 	std::map<uint32_t, std::vector<AtlasFrame> > m_animation;
-	
+
 };
 
 }
