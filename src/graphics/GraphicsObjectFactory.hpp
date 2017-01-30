cfb1de094d4635372741db9f622ee872889c5b00
Jose C. Rubio <jose.rubio@mindmaze.ch>
2017-01-30 16:46:43 +0100

    Changes cration of GraphicObjects with Configuration

--- src/graphics/GraphicsObjectFactory.hpp
+++ src/graphics/GraphicsObjectFactory.hpp
@@ -1,11 +1,7 @@
 #pragma once
 
 #include "GraphicsObject.hpp"
-#include <iostream>
-#include <fcntl.h>
-#include <fstream>
-#include <google/protobuf/text_format.h>
-#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <utils/Configuration.hpp>
 #include <graphics.pb.h>
 
 namespace pumpkin {
@@ -13,32 +9,21 @@ namespace pumpkin {
 class GraphicsObjectFactory {
 
 public:
+	typedef Configuration<voyage::GraphicsObjectCfg> Config;
 
-	// TODO: Bring this to an parent factory
-	GraphicsObjectFactory(std::string config_file) : m_config_file(config_file)  {
+	GraphicsObjectFactory(const std::string & config_file) :
+		m_config(Config(config_file)) {}
 
-		int fileDescriptor = open(m_config_file.c_str(), O_RDONLY);
-		google::protobuf::io::FileInputStream fileInput(fileDescriptor);
-		fileInput.SetCloseOnDelete( true );
-
-		if (!google::protobuf::TextFormat::Parse(&fileInput, &m_graphics_cfg))
-		{
-			std::cout << "Failed to parse file!" << std::endl;
-		}
-
-		//TODO: check atlas sanity (sizes make sense)
+	bool generate(GraphicsObject *go) {
+		go->loadTextureAtlas(m_config.config().atlas_id());
+		go->loadShader(m_config.config().shader_id());
+		go->m_width = m_config.config().width();
+		go->m_height = m_config.config().height();
 	}
-
-bool generate(GraphicsObject *go) {	
-	go->loadTextureAtlas(m_graphics_cfg.atlas_id());
-	go->loadShader(m_graphics_cfg.shader_id());
-	go->m_width = m_graphics_cfg.width();
-	go->m_height = m_graphics_cfg.height();
-}
 
 private:
 	std::string m_config_file;
-	voyage::GraphicsObjectCfg m_graphics_cfg;
+	Config m_config;
 };
 
 }
