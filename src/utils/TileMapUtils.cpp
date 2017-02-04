929757cabd004f57f172e49affe9fd26ec8227e5
byfron <josecrubio82@gmail.com>
2017-02-04 20:55:42 +0100

    Adds tilemap utils

---
+++ src/utils/TileMapUtils.cpp
@@ -0,0 +1,48 @@
+#include "TileMapUtils.hpp"
+
+namespace pumpkin {
+namespace TileMapUtils {
+
+	MeshType tileIdToMeshObjectType(uint32_t tile_id) {
+		switch(tile_id) {
+		case 0:
+			return pumpkin::MeshType::TILE_MESH;
+			break;
+		case 1:
+			return pumpkin::MeshType::WALL_MESH;
+			break;
+		}
+	}
+
+
+	std::vector<Vec2i> tileIdToAtlasFrameCoords(uint32_t tile_id) {
+
+		std::vector<Vec2i> coords;
+		
+		switch(tile_id) {
+		case 0:
+			coords.push_back(Vec2i(0,0));
+			break;
+
+		case 1:
+			coords.push_back(Vec2i(0,2));
+			coords.push_back(Vec2i(0,1));
+			break;
+
+		};
+
+		return coords;
+	}
+
+	Vec3f projectCameraPointInGround(const Vec3f & point) {
+
+		// cast a ray from camera and intersect with Z=0 plane
+		return Vec3f(point(0), point(1), 0.0f);
+	}
+			
+	Vec3f projectPointInGround(const Vec3f & point) {
+		return Vec3f(point(0), point(1), 0.0f);
+	}
+
+}
+}
--- src/utils/TileMapUtils.hpp
+++ src/utils/TileMapUtils.hpp
@@ -1,38 +1,13 @@
 #pragma once
 
+#include <common/math.hpp>
+#include "MeshFactory.hpp"
+
 namespace pumpkin {
 namespace TileMapUtils {
-
-	MeshType tileIdToMeshObjectType(uint32_t tile_id) {
-		switch(tile_id) {
-		case 0:
-			return MeshType::TILE_MESH;
-			break;
-		case 1:
-			return MeshType::WALL_MESH;
-			break;
-		}
-	}
-
-
-	std::vector<Vec2i> tileIdToAtlasFrameCoords(uint32_t tile_id) {
-
-		std::vector<Vec2i> coords;
-		
-		switch(tile_id) {
-		case 0:
-			coords.push_back(Vec2i(0,0));
-			break;
-
-		case 1:
-			coords.push_back(Vec2i(0,2));
-			coords.push_back(Vec2i(0,1));
-			break;
-
-		};
-
-		return coords;
-	}
-
+	MeshType tileIdToMeshObjectType(uint32_t tile_id);
+	std::vector<Vec2i> tileIdToAtlasFrameCoords(uint32_t tile_id);	
+	Vec3f projectCameraPointInGround(const Vec3f & point);
+	Vec3f projectPointInGround(const Vec3f & point);
 }
 }
