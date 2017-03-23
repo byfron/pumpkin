541dd524a3f27d77be9ac8f635f3973af5b37ada
Jose C. Rubio <jose.rubio@mindmaze.ch>
2017-03-23 15:49:31 +0100

    Cleans up a bit

--- src/graphics/Scene.cpp
+++ src/graphics/Scene.cpp
@@ -34,15 +34,7 @@ void Scene::update(float d) {
 	Eigen::AngleAxis<float> aa(3.1416, Vec3f(0.0,1.0,0.0));
 	Eigen::MatrixXf rot = Eigen::MatrixXf::Identity(4,4);
 	rot.block(0,0,3,3) = aa.matrix();
-//	m_transform = rot;
 	meshSubmit();
-
-	//bgfx::setVertexBuffer(m_vbh);
-	//bgfx::setIndexBuffer(m_ibh);
-	//bgfx::setTexture(0, u_texColor,  m_texture->getColorHandle());
-
-	// Submit primitive for rendering to view 0.
-	//bgfx::submit(0, m_shader->getHandle());
 }
 
 }
